8e95805611e356fbf231c65e01e42d3e18dfafee
tsaryapkin_aa <tsaryapkin_aa@teleformis.ru>
2019-09-29 14:06:42 +0300

    ldap

--- src/main/java/com/bss/kerberosspring/KerberosSpringApplication.java
+++ src/main/java/com/bss/kerberosspring/KerberosSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 @SpringBootApplication
 public class KerberosSpringApplication {
